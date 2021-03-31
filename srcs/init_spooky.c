64bdee88661d9fd08cc7ccd96102476350e2998a
Epicurius <niklas0702@gmail.com>
2021-03-31 13:52:50 +0300

    fixed spooky texture cordinates

--- srcs/init_spooky.c
+++ srcs/init_spooky.c
@@ -8,9 +8,9 @@ void	init_spooky_nb(t_texture_sheet *sprite)
 	sprite->nb[MOVE][FRAMES] = 6;
 	sprite->nb[MOVE][ANGLES] = 8;
 	sprite->nb[ATTACK][FRAMES] = 6;
-	sprite->nb[ATTACK][ANGLES] = 8;
-	sprite->nb[DEATH][FRAMES] = 0;//7;
-	sprite->nb[DEATH][ANGLES] = 0;//1;
+	sprite->nb[ATTACK][ANGLES] = 1;
+	sprite->nb[DEATH][FRAMES] = 6;
+	sprite->nb[DEATH][ANGLES] = 1;
 }
 
 int	init_spooky(t_doom *doom, t_texture_sheet *sprite)
@@ -50,61 +50,80 @@ int	init_spooky(t_doom *doom, t_texture_sheet *sprite)
 	sprite->pos[0][0][2] = new_rect(968,  1153, 1113, 1350);
 	sprite->pos[0][0][3] = new_rect(1132, 1153, 1265, 1350);
 
-	sprite->pos[1][0][4] = new_rect(25,   1153, 139,  1350);
-	sprite->pos[1][0][5] = new_rect(172,  1153, 308,  1350);
-	sprite->pos[1][0][6] = new_rect(325,  1153, 471,  1350);
-	sprite->pos[1][0][7] = new_rect(494,  1153, 618,  1350);
-	sprite->pos[1][0][0] = new_rect(654,  1153, 775,  1350);
-	sprite->pos[1][0][1] = new_rect(821,  1153, 945,  1350);
-	sprite->pos[1][0][2] = new_rect(968,  1153, 1113, 1350);
-	sprite->pos[1][0][3] = new_rect(1132, 1153, 1265, 1350);
+	sprite->pos[1][0][4] = new_rect(25,   1163, 139,  1350);
+	sprite->pos[1][0][5] = new_rect(172,  1163, 308,  1350);
+	sprite->pos[1][0][6] = new_rect(325,  1163, 471,  1350);
+	sprite->pos[1][0][7] = new_rect(494,  1163, 618,  1350);
+	sprite->pos[1][0][0] = new_rect(654,  1163, 775,  1350);
+	sprite->pos[1][0][1] = new_rect(821,  1163, 945,  1350);
+	sprite->pos[1][0][2] = new_rect(968,  1163, 1113, 1350);
+	sprite->pos[1][0][3] = new_rect(1132, 1163, 1265, 1350);
 
-	sprite->pos[1][1][4] = new_rect(0,    1350, 160,  1542);
-	sprite->pos[1][1][5] = new_rect(160,  1350, 320,  1542);
-	sprite->pos[1][1][6] = new_rect(320,  1350, 480,  1542);
-	sprite->pos[1][1][7] = new_rect(480,  1350, 640,  1542);
-	sprite->pos[1][1][0] = new_rect(640,  1350, 800,  1542);
-	sprite->pos[1][1][1] = new_rect(960,  1350, 1020, 1542);
-	sprite->pos[1][1][2] = new_rect(1020, 1350, 1180, 1542);
-	sprite->pos[1][1][3] = new_rect(1180, 1350, 1280, 1542);
+	sprite->pos[1][1][4] = new_rect(25,   1356, 139,  1542);
+	sprite->pos[1][1][5] = new_rect(172,  1356, 308,  1542);
+	sprite->pos[1][1][6] = new_rect(325,  1356, 471,  1542);
+	sprite->pos[1][1][7] = new_rect(494,  1356, 618,  1542);
+	sprite->pos[1][1][0] = new_rect(654,  1356, 775,  1542);
+	sprite->pos[1][1][1] = new_rect(821,  1356, 945,  1542);
+	sprite->pos[1][1][2] = new_rect(968,  1356, 1113, 1542);
+	sprite->pos[1][1][3] = new_rect(1132, 1356, 1265, 1542);
 
-	sprite->pos[1][2][4] = new_rect(0,    1542, 160,  1734);
-	sprite->pos[1][2][5] = new_rect(160,  1542, 320,  1734);
-	sprite->pos[1][2][6] = new_rect(320,  1542, 480,  1734);
-	sprite->pos[1][2][7] = new_rect(480,  1542, 640,  1734);
-	sprite->pos[1][2][0] = new_rect(640,  1542, 800,  1734);
-	sprite->pos[1][2][1] = new_rect(960,  1542, 1020, 1734);
-	sprite->pos[1][2][2] = new_rect(1020, 1542, 1180, 1734);
-	sprite->pos[1][2][3] = new_rect(1180, 1542, 1280, 1734);
+	sprite->pos[1][2][4] = new_rect(25,   1550, 139,  1734);
+	sprite->pos[1][2][5] = new_rect(172,  1550, 308,  1734);
+	sprite->pos[1][2][6] = new_rect(325,  1550, 471,  1734);
+	sprite->pos[1][2][7] = new_rect(494,  1550, 618,  1734);
+	sprite->pos[1][2][0] = new_rect(654,  1550, 775,  1734);
+	sprite->pos[1][2][1] = new_rect(821,  1550, 945,  1734);
+	sprite->pos[1][2][2] = new_rect(968,  1550, 1113, 1734);
+	sprite->pos[1][2][3] = new_rect(1132, 1550, 1265, 1734);
 
-	sprite->pos[1][3][4] = new_rect(0,    1734, 160,  1926);
-	sprite->pos[1][3][5] = new_rect(160,  1734, 320,  1926);
-	sprite->pos[1][3][6] = new_rect(320,  1734, 480,  1926);
-	sprite->pos[1][3][7] = new_rect(480,  1734, 640,  1926);
-	sprite->pos[1][3][0] = new_rect(640,  1734, 800,  1926);
-	sprite->pos[1][3][1] = new_rect(960,  1734, 1020, 1926);
-	sprite->pos[1][3][2] = new_rect(1020, 1734, 1180, 1926);
-	sprite->pos[1][3][3] = new_rect(1180, 1734, 1280, 1926);
+	sprite->pos[1][3][4] = new_rect(25,   1742, 139,  1928);
+	sprite->pos[1][3][5] = new_rect(172,  1742, 308,  1928);
+	sprite->pos[1][3][6] = new_rect(325,  1742, 471,  1928);
+	sprite->pos[1][3][7] = new_rect(494,  1742, 618,  1928);
+	sprite->pos[1][3][0] = new_rect(654,  1742, 775,  1928);
+	sprite->pos[1][3][1] = new_rect(821,  1742, 945,  1928);
+	sprite->pos[1][3][2] = new_rect(968,  1742, 1113, 1928);
+	sprite->pos[1][3][3] = new_rect(1132, 1742, 1265, 1928);
 
-	sprite->pos[1][4][4] = new_rect(0,    1926, 160,  2118);
-	sprite->pos[1][4][5] = new_rect(160,  1926, 320,  2118);
-	sprite->pos[1][4][6] = new_rect(320,  1926, 480,  2118);
-	sprite->pos[1][4][7] = new_rect(480,  1926, 640,  2118);
-	sprite->pos[1][4][0] = new_rect(640,  1926, 800,  2118);
-	sprite->pos[1][4][1] = new_rect(960,  1926, 1020, 2118);
-	sprite->pos[1][4][2] = new_rect(1020, 1926, 1180, 2118);
-	sprite->pos[1][4][3] = new_rect(1180, 1926, 1280, 2118);
+	sprite->pos[1][4][4] = new_rect(25,   1938, 139,  2119);
+	sprite->pos[1][4][5] = new_rect(172,  1938, 308,  2119);
+	sprite->pos[1][4][6] = new_rect(325,  1938, 471,  2119);
+	sprite->pos[1][4][7] = new_rect(494,  1938, 618,  2119);
+	sprite->pos[1][4][0] = new_rect(654,  1938, 775,  2119);
+	sprite->pos[1][4][1] = new_rect(821,  1938, 945,  2119);
+	sprite->pos[1][4][2] = new_rect(968,  1938, 1113, 2119);
+	sprite->pos[1][4][3] = new_rect(1132, 1938, 1265, 2119);
 
-	sprite->pos[1][5][4] = new_rect(0,    2118, 160,  2310);
-	sprite->pos[1][5][5] = new_rect(160,  2118, 320,  2310);
-	sprite->pos[1][5][6] = new_rect(320,  2118, 480,  2310);
-	sprite->pos[1][5][7] = new_rect(480,  2118, 640,  2310);
-	sprite->pos[1][5][0] = new_rect(640,  2118, 800,  2310);
-	sprite->pos[1][5][1] = new_rect(960,  2118, 1020, 2310);
-	sprite->pos[1][5][2] = new_rect(1020, 2118, 1180, 2310);
-	sprite->pos[1][5][3] = new_rect(1180, 2118, 1280, 2310);
+	sprite->pos[1][5][4] = new_rect(25,   2128, 139,  2313);
+	sprite->pos[1][5][5] = new_rect(172,  2128, 308,  2313);
+	sprite->pos[1][5][6] = new_rect(325,  2128, 471,  2313);
+	sprite->pos[1][5][7] = new_rect(494,  2128, 618,  2313);
+	sprite->pos[1][5][0] = new_rect(654,  2128, 775,  2313);
+	sprite->pos[1][5][1] = new_rect(821,  2128, 945,  2313);
+	sprite->pos[1][5][2] = new_rect(968,  2128, 1113, 2313);
+	sprite->pos[1][5][3] = new_rect(1132, 2128, 1265, 2313);
 
 
+
+	sprite->pos[2][0][0] = new_rect(0,    0,   160,  195);
+	sprite->pos[2][1][0] = new_rect(0,    195, 160,  375);
+	sprite->pos[2][2][0] = new_rect(0,    375, 160,  563);
+	sprite->pos[2][3][0] = new_rect(0,    584, 160,  766);
+	sprite->pos[2][4][0] = new_rect(0,    774, 160,  966);
+	sprite->pos[2][5][0] = new_rect(0,    972, 160,  1152);
+
+
+	sprite->pos[3][0][0] = new_rect(6,    2320, 145,  2496);
+	sprite->pos[3][1][0] = new_rect(150,  2333, 284,  2496);
+	sprite->pos[3][2][0] = new_rect(305,  2320, 432,  2423);
+	sprite->pos[3][3][0] = new_rect(438,  2320, 583,  2412);
+	sprite->pos[3][4][0] = new_rect(582,  2320, 759,  2406);
+	sprite->pos[3][5][0] = new_rect(774,  2320, 941,  2066);
+
+
+
+/*
 	sprite->pos[2][0][4] = new_rect(0,    0, 160,  195);
 	sprite->pos[2][0][5] = new_rect(160,  0, 320,  195);
 	sprite->pos[2][0][6] = new_rect(320,  0, 480,  195);
@@ -158,6 +177,6 @@ int	init_spooky(t_doom *doom, t_texture_sheet *sprite)
 	sprite->pos[2][5][1] = new_rect(960,  972, 1020, 1152);
 	sprite->pos[2][5][2] = new_rect(1020, 972, 1180, 1152);
 	sprite->pos[2][5][3] = new_rect(1180, 972, 1280, 1152);
-
+*/
 	return (1);
 }
