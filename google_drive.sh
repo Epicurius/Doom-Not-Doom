037bca76a3cf782d1f2c9c32099af1aa04afc142
Epicurius <nneronin@student.hive.fi>
2021-05-05 13:35:56 +0300

    new google_drive link id

--- google_drive.sh
+++ google_drive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fileid="1_3I3q8LEM5U7CgVcFtMpDLtk0qM1mjtF"
+fileid="1u4PdnYhxmeqmOUZWFHRCKPj78ECJ8NzK"
 filename="file.tar.gz"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
