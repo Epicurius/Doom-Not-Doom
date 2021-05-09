b4fecc9c2bcf447c38fe522d3461669ff0ac6ec5
Epicurius <nneronin@student.hive.fi>
2021-05-09 18:30:03 +0300

    added new textures and sounds and updated drive link

--- google_drive.sh
+++ google_drive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fileid="1u4PdnYhxmeqmOUZWFHRCKPj78ECJ8NzK"
+fileid="1wfvCPk4apsCk6Dv-GC58aJaflCc-Rct7"
 filename="file.tar.gz"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
