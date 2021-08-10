2a1bac57e6f12a85b1eed81f9a346d128d1b9a38
Epicurius <nneronin@student.hive.fi>
2021-08-10 11:49:52 +0300

    vertical collision rework

--- srcs/vertical_collision.c
+++ srcs/vertical_collision.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 14:52:17 by nneronin          #+#    #+#             */
-/*   Updated: 2021/08/10 10:01:42 by nneronin         ###   ########.fr       */
+/*   Updated: 2021/08/10 11:46:32 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ static int	portal_cliff(t_doom *doom, t_motion *motion, t_v3 where)
 {
 	int		i;
 	t_v3	point;
-	double	dist;
 	t_wall	*wall;
 
 	i = -1;
@@ -26,45 +25,40 @@ static int	portal_cliff(t_doom *doom, t_motion *motion, t_v3 where)
 		if (wall->solid || wall->n == -1)
 			continue ;
 		point = closest_point_on_segment_v2(where, wall->v1, wall->v2);
-		dist = point_distance_v2(point.x, point.y, where.x, where.y);
-		
-		if (dist <= DIAMETER && where.z <= floor_at(&doom->sectors[wall->n], point))
+		if (point_distance_v2(point.x, point.y, where.x, where.y) > DIAMETER)
+			continue ;
+		if (where.z + motion->velocity.z
+			<= floor_at(&doom->sectors[wall->n], point))
 		{
 			if (motion->velocity.z < 0)
 				motion->velocity.z = 0;
 			return (1);
 		}
-		if (dist <= DIAMETER && where.z + motion->height + motion->velocity.z
+		if (where.z + motion->height + motion->velocity.z
 			>= ceiling_at(&doom->sectors[wall->n], point))
 		{
 			motion->velocity.z = 0;
 			return (1);
 		}
-	}
+	}	
 	return (0);
 }
 
 int	vertical_collision(t_doom *doom, t_motion *motion)
 {
-	t_fc	y;
+	t_fc	where;
 
-	y.ceiling = ceiling_at(&doom->sectors[motion->curr_sect], motion->where);
-	y.floor = floor_at(&doom->sectors[motion->curr_sect], motion->where);
-	if (y.ceiling - y.floor < motion->height)
+	where.ceiling = ceiling_at(&doom->sectors[motion->curr_sect], motion->where);
+	where.floor = floor_at(&doom->sectors[motion->curr_sect], motion->where);
+	if (where.ceiling - where.floor < motion->height)
 		return (1);
-	if (!portal_cliff(doom, motion, motion->where) && !motion->flight && motion->where.z > y.floor)
+	if (motion->flight == FALSE && motion->where.z > where.floor
+		&& !portal_cliff(doom, motion, motion->where))
 	{
 		motion->velocity.z -= doom->sectors[motion->curr_sect].gravity;
 		motion->where.z += motion->velocity.z;
 	}
-	// if breaks enable this || motion->where.z + motion->velocity.z < y.floor)
-	if (motion->where.z < y.floor)
-	{
-		motion->velocity.z = 0;
-		if (motion->where.z < y.floor)
-			motion->where.z = y.floor;
-	}
-	if (motion->velocity.z > 0 && motion->where.z + motion->velocity.z + motion->height >= y.ceiling)
+	if (motion->where.z + motion->velocity.z + motion->height >= where.ceiling)
 		motion->velocity.z = 0;
 	motion->move.z += motion->velocity.z;
 	return (0);
