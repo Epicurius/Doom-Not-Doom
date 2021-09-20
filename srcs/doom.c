e453765790274112b83975063691a7df733f62ad
Epicurius <nneronin@student.hive.fi>
2021-09-20 17:42:47 +0300

    Found the root of all the issues, if fps goes over 100 things start to break

--- srcs/doom.c
+++ srcs/doom.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 11:32:08 by nneronin          #+#    #+#             */
-/*   Updated: 2021/09/19 16:04:54 by nneronin         ###   ########.fr       */
+/*   Updated: 2021/09/20 17:41:29 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,9 +95,6 @@ int	main(int ac, char **av)
 {
 	t_settings	settings;
 	
-	ft_printf("%d\n", sizeof(t_wall));
-	ft_printf("%d\n", sizeof(t_bullet_hole));
-	ft_printf("%d\n", sizeof(t_wsprite));
 	if (ac == 1)
 		print_help_msg();
 	args(ac, av, &settings);
