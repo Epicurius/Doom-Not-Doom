a4bd43e828ca2b965bb2cc3cc1ba1d630b199a59
Epicurius <nneronin@student.hive.fi>
2021-12-11 10:18:14 +0200

    fixed some stuff

--- srcs/precompute_buy_menu.c
+++ srcs/precompute_buy_menu.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/24 15:34:12 by nneronin          #+#    #+#             */
-/*   Updated: 2021/12/10 17:50:16 by nneronin         ###   ########.fr       */
+/*   Updated: 2021/12/11 09:52:36 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@
  */
 void	precompute_buymenu(t_doom *doom)
 {
+	int	x;
+	int	y;
+
 	Mix_PlayChannel(CHANNEL_MUSIC, doom->sound[WAV_ELEVATOR_MUSIC], -1);
+	SDL_GetMouseState(&x, &y);
 	SDL_SetRelativeMouseMode(SDL_FALSE);
 	buymenu(doom->win, doom->surface, &doom->inv);
 	Mix_PlayChannel(-1, doom->sound[WAV_BIP], 0);
 	ft_bzero(&doom->keys, 517);
 	doom->player.action = NONE;
+	SDL_WarpMouseInWindow(doom->win, x, y);
 	SDL_SetRelativeMouseMode(SDL_TRUE);
 	doom->time.curr = SDL_GetTicks();
 	Mix_PlayChannel(CHANNEL_MUSIC, doom->sound[WAV_MAIN_THEME], -1);
