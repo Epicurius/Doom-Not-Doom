2173ac7648c77e219762b9a0164d24948657189d
Niklas Neronin <niklas.neronin@gmail.com>
2023-09-19 13:48:10 +0300

    new log
    Signed-off-by: Niklas Neronin <niklas.neronin@gmail.com>

--- libs/liblg/liblg.h
+++ libs/liblg/liblg.h
@@ -8,12 +8,18 @@
 #ifndef LIBLG_H
 # define LIBLG_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <stdarg.h>
-# include <stdio.h>
+# include <pthread.h>
 
-# define LOG_BUFFER 1024
+# define LG_USE_LIBPF
+
+#ifdef LG_USE_LIBPF
+# include "../libpf/libpf.h"
+# include "../libft/libft.h"
+#else
+# include <stdio.h>
+# include <stdarg.h>
+# include <stdlib.h>
+#endif
 
 enum
 {
@@ -25,38 +31,28 @@ enum
 
 # define LG_PRINT_STD	stderr 
 
-# define LG_DEBUG(...)	lg_log(LEVEL_DEBUG, __TIME__, __FILE__, __FUNCTION__, \
-							   __LINE__, __VA_ARGS__)
-# define LG_INFO(...)	lg_log(LEVEL_INFO,	__TIME__, __FILE__, __FUNCTION__, \
-							   __LINE__, __VA_ARGS__)
-# define LG_WARN(...)	lg_log(LEVEL_WARN,	__TIME__, __FILE__, __FUNCTION__, \
-							   __LINE__, __VA_ARGS__)
-# define LG_ERROR(...)	lg_log(LEVEL_ERROR, __TIME__, __FILE__, __FUNCTION__, \
-							   __LINE__, __VA_ARGS__)
-
-typedef struct s_lgEvent
-{
-	va_list		ap;
-	const char	*fmt;
-	const char	*time;
-	const char	*file;
-	const char	*func;
-	int			line;
-	int			lvl;
-}				lg_event;
+# define LG_DEBUG(...)	lg_log(LEVEL_DEBUG, __TIME__, __FILE__, __LINE__, __VA_ARGS__)
+# define LG_INFO(...)	lg_log(LEVEL_INFO, __TIME__, __FILE__, __LINE__, __VA_ARGS__)
+# define LG_WARN(...)	lg_log(LEVEL_WARN, __TIME__, __FILE__, __LINE__, __VA_ARGS__)
+# define LG_ERROR(...)	lg_log(LEVEL_ERROR, __TIME__, __FILE__, __LINE__, __VA_ARGS__)
 
 typedef struct g_log
 {
-	FILE		*fp;
-	int			stdlvl;
-	int			filelvl;
+	FILE			*fp;
+	int				stdlvl;
+	int				filelvl;
+	pthread_mutex_t	mutex;
 }				g_log;
 
 /* File: ./liblg.c */
-int			lg_openFile(char *file, char *mode);
-void		lg_addFile(FILE *fp);
-void		lg_closeFile(void);
 void		lg_setLevel(int std, int file);
+int			lg_setFile(FILE *fp);
+int			lg_openFile(char *file, char *mode);
+void		lg_closeFile(void);
+void		lg_fwrite(int lvl, const char *file, int line, const char *time,
+					  const char *fmt, va_list ap);
+void		lg_write(int lvl, const char *file, int line,
+					 const char *fmt, va_list ap);
 void		lg_log(int lvl, const char *time, const char *file,
-				   const char *func, int line, const char *fmt, ...);
+				   int line, const char *fmt, ...);
 #endif
--- libs/liblg/srcs/liblg.c
+++ libs/liblg/srcs/liblg.c
@@ -7,75 +7,116 @@
 
 #include "liblg.h"
 
-struct g_log	lg_global = {NULL, -1, -1};
+static const char *lg_levels[] = {
+	"DEBUG",
+	"INFO",
+	"WARN",
+	"ERROR"
+};
+
+static const char *lg_colors[] = {
+	"\x1b[36m",
+	"\x1b[32m",
+	"\x1b[33m",
+	"\x1b[31m"
+};
+
+struct g_log	lg_global = {
+	.fp=NULL,
+	.stdlvl=-1,
+	.filelvl=-1,
+	.mutex=PTHREAD_MUTEX_INITIALIZER
+};
+
+int	lg_setFile(FILE *fp)
+{
+	if (lg_global.fp && fp != NULL) {
+		LG_INFO("File already set.");
+		return (0);
+	}
+
+	lg_global.fp = fp;
+	return (1);
+}
 
 int	lg_openFile(char *file, char *mode)
 {
 	lg_global.fp = fopen(file, mode);
 	if (lg_global.fp == NULL)
 		return (0);
-	LG_INFO("Log Opened.");
-	return (1);
-}
 
-void	lg_addFile(FILE *fp)
-{
-	lg_global.fp = fp;
+	return (1);
 }
 
 void	lg_closeFile(void)
 {
-	LG_INFO("Log Closed.");
+	if (lg_global.fp == NULL)
+		return ;
+
 	fclose(lg_global.fp);
 }
 
 void	lg_setLevel(int std, int file)
 {
-	if (std < 3)
-		lg_global.stdlvl = std;
-	if (file < 3)
-		lg_global.filelvl = file;
+	lg_global.stdlvl = std;
+	lg_global.filelvl = file;
 }
 
-static void	lg_write(lg_event *ev)
+void	lg_fwrite(int lvl, const char *file, int line, const char *time,
+	const char *fmt, va_list ap)
 {
-	char		buff[LOG_BUFFER];
-	const char	*l[] = {"DEBUG", "INFO", "WARN", "ERROR"};
-	const char	*c[] = {"\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m"};
-
-	vsnprintf(buff, LOG_BUFFER, ev->fmt, ev->ap);
-	if (ev->lvl >= lg_global.stdlvl)
-	{
-		fprintf(LG_PRINT_STD, "%s[%-5s]\x1b[0m \x1b[90m%s:%d\x1b[0m : ",
-			c[ev->lvl], l[ev->lvl], ev->func, ev->line);
-		fprintf(LG_PRINT_STD, "%s\n", buff);
-		fflush(LG_PRINT_STD);
-	}
-	if (ev->lvl >= lg_global.filelvl)
-	{
-		fprintf(lg_global.fp, "%s %-5s:\t\t%s\t\t", ev->time, l[ev->lvl], buff);
-		fprintf(lg_global.fp, "[%s : %s : %d]\n", ev->file, ev->func, ev->line);
-		fflush(lg_global.fp);
-	}
+	pthread_mutex_lock(&lg_global.mutex);
+#ifdef LG_USE_LIBPF
+	ft_fprintf(lg_global.fp, "%s %-5s %s:%d\n\t",
+		time, lg_levels[lvl], file, line);
+	ft_vfprintf(lg_global.fp, fmt, ap);
+#else
+	fprintf(lg_global.fp, "%s %-5s %s:%d\n\t",
+		time, lg_levels[lvl], file, line);
+	vfprintf(lg_global.fp, fmt, ap);
+	fflush(lg_global.fp);
+#endif
+	fwrite("\n", 1, 1, lg_global.fp);
+	pthread_mutex_unlock(&lg_global.mutex);
 }
 
-void	lg_log(int lvl, const char *time, const char *file, const char *func,
-	int line, const char *fmt, ...)
+void	lg_write(int lvl, const char *file, int line, const char *fmt,
+	va_list ap)
 {
-	lg_event	ev;
+	pthread_mutex_lock(&lg_global.mutex);
+#ifdef LG_USE_LIBPF
+	ft_fprintf(LG_PRINT_STD, "\n%s%-5s\x1b[0m \x1b[90m%s:%d\x1b[0m : ",
+		lg_colors[lvl], lg_levels[lvl], file, line);
+	ft_vfprintf(LG_PRINT_STD, fmt, ap);
+#else
+	fprintf(LG_PRINT_STD, "\n%s%-5s\x1b[0m \x1b[90m%s:%d\x1b[0m : ",
+		lg_colors[lvl], lg_levels[lvl], file, line);
+	vfprintf(LG_PRINT_STD, fmt, ap);
+	fflush(LG_PRINT_STD);
+#endif
+	fwrite("\n", 1, 1, LG_PRINT_STD);
+	pthread_mutex_unlock(&lg_global.mutex);
+}
 
-	if ((lg_global.fp && lvl >= lg_global.filelvl) || lvl >= lg_global.stdlvl)
+void	lg_log(int lvl, const char *time, const char *file, int line,
+	const char *fmt, ...)
+{
+	va_list ap;
+	if (lvl >= lg_global.stdlvl)
 	{
-		ev.fmt = fmt;
-		ev.time = time;
-		ev.file = file;
-		ev.func = func;
-		ev.line = line;
-		ev.lvl = lvl;
-		va_start(ev.ap, fmt);
-		lg_write(&ev);
-		va_end(ev.ap);
+		va_start(ap, fmt);
+		lg_write(lvl, file, line, fmt, ap);
+		va_end(ap);
 	}
-	if (lvl == LEVEL_ERROR)
+	if (lg_global.fp && lvl >= lg_global.filelvl)
+	{
+		va_start(ap, fmt);
+		lg_fwrite(lvl, file, line, time, fmt, ap);
+		va_end(ap);
+	}
+
+	if (lvl == LEVEL_ERROR) {
+		lg_closeFile();
 		exit (1);
+	}
 }
